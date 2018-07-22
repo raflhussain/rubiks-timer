3fae9e028693bdf099b25b0cdb5f60c6a857f8ea
raflhussain <raflkussayhussain@gmail.com>
2018-07-22 12:00:56 -0400

    timer updated

--- rubikstimer.sh
+++ rubikstimer.sh
@@ -1,11 +1,30 @@
-#!/bin/bash
-date1=`date +%s`; while true; do 
-echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%M:%S)\r";
-   done
-read -s -n 1 key
+BEGIN=$(date +%s)
 
-if [[ $key = "" ]]; then
-    echo 'Enter pressed'
-fi
+echo Starting Rubiks Timer for $USER...
+echo Solve cube, press any key when finished.
+echo
 
-# TODO: echo/log time
+while true; do
+    NOW=$(date +%s)
+    let DIFF=$(($NOW - $BEGIN))
+    let MINS=$(($DIFF / 60))
+    let SECS=$(($DIFF % 60))
+    let TIME=$((($MINS * 60) + $SECS))
+
+    # \r  is a "carriage return" - returns cursor to start of line
+    printf "\r%02d:%02d" $MINS $SECS
+
+    # In the following line -t for timeout, -N for just 1 character
+    read -t 0.25 -N 1 input
+    # Any key pressed (except spacebar/enter)
+    if [[ $input  ]]; then
+        # Output time to file
+        echo $USER $TIME >> solvetimes.txt
+        echo
+        # Reintroduce prompt on new line
+        echo
+        echo Time: $MINS minutes, $SECS seconds
+        echo
+        break 
+    fi
+done
